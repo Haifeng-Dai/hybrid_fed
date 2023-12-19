2b756c38704af99da04e91352994a9c32e18bfaf
Haifeng-Dai <1655154608@qq.com>
2023-12-20 01:17:16 +0800

    use deepcopy instand of term itself

--- utils/lib_util.py
+++ utils/lib_util.py
@@ -1,8 +1,23 @@
+from copy import deepcopy
+
 def get_list(len_list, term=[]):
     '''
     返回一个全是空列表的列表
     '''
     list_return = []
+    # if not term:
+    #     term = []
     for _ in range(len_list):
-        list_return.append(term)
+        list_return.append(deepcopy(term))
     return list_return
+
+
+if __name__ == '__main__' :
+    # deepcopy is importent
+    a = get_list(2)
+    print(a)
+    for i, _ in enumerate(a):
+        a[i].append(i)
+    print(a)
+    b = get_list(3)
+    print(b)
