f43a5a9220ea7f962a977d9d07f4fb74da3e9239
Haifeng-Dai <1655154608@qq.com>
2024-01-13 20:22:16 +0800

    u

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
 import torch
+import torchvision
 
-a = torch.tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype=torch.float)
-
-print(a.mean())
-print(torch.nn.MSELoss()(a, torch.zeros_like(a)))
+net1 = torchvision.models.resnet18(weights=None, num_classes=10)
+net2 = torchvision.models.resnet34(weights=None, num_classes=10)
+net3 = torchvision.models.resnet50(weights=None, num_classes=10)
+net4 = torchvision.models.resnet101(weights=None, num_classes=10)
+net5 = torchvision.models.resnet152(weights=None, num_classes=10)
+net6 = torchvision.models.vgg11_bn(weights=None, num_classes=10)
+print(len(net1.state_dict().keys()))
+print(len(net2.state_dict().keys()))
+print(len(net3.state_dict().keys()))
+print(len(net4.state_dict().keys()))
+print(len(net5.state_dict().keys()))
+print(len(net6.state_dict().keys()))
